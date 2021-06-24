9e0b2bd6120f32c4babced7c848720b8ffdddeb2
ntantai21810 <nguyentantai9a3@gmail.com>
2021-06-25 05:01:51 +0700

    Init commit

---
+++ userprog/exception.cc
@@ -0,0 +1,146 @@
+// exception.cc 
+//	Entry point into the Nachos kernel from user programs.
+//	There are two kinds of things that can cause control to
+//	transfer back to here from user code:
+//
+//	syscall -- The user code explicitly requests to call a procedure
+//	in the Nachos kernel.  Right now, the only function we support is
+//	"Halt".
+//
+//	exceptions -- The user code does something that the CPU can't handle.
+//	For instance, accessing memory that doesn't exist, arithmetic errors,
+//	etc.  
+//
+//	Interrupts (which can also cause control to transfer from user
+//	code into the Nachos kernel) are handled elsewhere.
+//
+// For now, this only handles the Halt() system call.
+// Everything else core dumps.
+//
+// Copyright (c) 1992-1993 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#include "copyright.h"
+#include "system.h"
+#include "syscall.h"
+
+//----------------------------------------------------------------------
+// ExceptionHandler
+// 	Entry point into the Nachos kernel.  Called when a user program
+//	is executing, and either does a syscall, or generates an addressing
+//	or arithmetic exception.
+//
+// 	For system calls, the following is the calling convention:
+//
+// 	system call code -- r2
+//		arg1 -- r4
+//		arg2 -- r5
+//		arg3 -- r6
+//		arg4 -- r7
+//
+//	The result of the system call, if any, must be put back into r2. 
+//
+// And don't forget to increment the pc before returning. (Or else you'll
+// loop making the same system call forever!
+//
+//	"which" is the kind of exception.  The list of possible exceptions 
+//	are in machine.h.
+//----------------------------------------------------------------------
+
+void IncreaseProgramCounter() 
+{
+	machine->registers[PrevPCReg] = machine->registers[PCReg]; 
+	machine->registers[PCReg] = machine->registers[NextPCReg];
+	machine->registers[NextPCReg] += 4;
+	
+}
+
+
+char* User2System(int virtAddr,int limit)
+{
+	int i;// index
+	int oneChar;
+	char* kernelBuf = NULL;
+	kernelBuf = new char[limit +1];//need for terminal string
+	if (kernelBuf == NULL)
+		return kernelBuf;
+	memset(kernelBuf,0,limit+1);
+	//printf("\n Filename u2s:");
+	for (i = 0 ; i < limit ;i++)
+	{
+		machine->ReadMem(virtAddr+i,1,&oneChar);
+		kernelBuf[i] = (char)oneChar;
+		//printf("%c",kernelBuf[i]);
+		if (oneChar == 0)
+		break;
+	}
+	return kernelBuf;
+}
+
+
+void
+ExceptionHandler(ExceptionType which)
+{
+    int type = machine->ReadRegister(2);
+		int MaxFileLength = 32;
+
+    switch (which) {
+			case NoException:
+				return;
+			case SyscallException: {
+				switch (type){
+					case SC_Halt: {
+						DEBUG('a', "\n Shutdown, initiated by user program.");
+						printf ("\n\n Shutdown, initiated by user program.");
+						interrupt->Halt();
+						break;
+					}
+					case SC_CreateFile: {
+						int virtAddr;
+						char* filename;
+						DEBUG('a',"\n SC_CreateFile call ...");
+						DEBUG('a',"\n Reading virtual address of filename");
+						// Lấy tham số tên tập tin từ thanh ghi r4
+						virtAddr = machine->ReadRegister(4);
+						DEBUG ('a',"\n Reading filename.");
+						// MaxFileLength là = 32
+						filename = User2System(virtAddr,MaxFileLength+1);
+						if (filename == NULL)
+						{
+							printf("\n Not enough memory in system");
+							DEBUG('a',"\n Not enough memory in system");
+							machine->WriteRegister(2,-1); // trả về lỗi cho chương
+							// trình người dùng
+							delete filename;
+							return;
+						}
+						DEBUG('a',"\n Finish reading filename.");
+						//DEBUG(‘a’,"\n File name : '"<<filename<<"'");
+						// Create file with size = 0
+						// Dùng đối tượng fileSystem của lớp OpenFile để tạo file,
+						// việc tạo file này là sử dụng các thủ tục tạo file của hệ điều
+						// hành Linux, chúng ta không quản ly trực tiếp các block trên
+						// đĩa cứng cấp phát cho file, việc quản ly các block của file
+						// trên ổ đĩa là một đồ án khác
+						if (!fileSystem->Create(filename,0))
+						{
+							printf("\n Error create file '%s'",filename);
+							machine->WriteRegister(2,-1);
+							delete filename;
+							return;
+						}
+						machine->WriteRegister(2,0); // trả về cho chương trình
+						// người dùng thành công
+						delete filename;
+						break;
+						}
+						default: {
+							printf("\n Unexpected user mode exception (%d %d)", which,
+							type);
+							interrupt->Halt();
+						}
+				}
+			}
+		}
+}
